8a4a9e32582f73993a9fb5437ebb707ed6c75687
Jhuly Bastos <jhulytbds@gmail.com>
2024-09-20 10:41:10 -0300

    navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Search, ChevronDown } from "lucide-react";
+import { Search, ChevronDown, LogOut } from "lucide-react";
 
 import Image from "next/image";
 import Logo from "../../public/img/logo.png";
@@ -15,6 +15,7 @@ import { SearchInput } from "@/components/SearchInput";
 import { CardBiografia } from "@/components/Biografia";
 import CardFriends from "@/components/Friends";
 import CardCommunity from "@/components/Community";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 
 export default function Home() {
   const router = useRouter();
@@ -60,7 +61,29 @@ export default function Home() {
             alt="imagem de perfil"
           />
           <h1>Jhuly Bastos</h1>
-          <ChevronDown color="#ED2590" />
+          <NavigationMenu.Root className="relative z-[1] flex justify-center">
+            <NavigationMenu.List className="font-manrope flex gap-16 text-black">
+              <NavigationMenu.Item className="flex items-center gap-1">
+                <NavigationMenu.Trigger className="relative flex items-center gap-1 group hover:text-dark-30 ">
+                  <ChevronDown
+                    color="#ED2590"
+                    className="transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                  />
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content className=" xl:w-auto xl:h-auto bg-white drop-shadow-md  mt-4  text-black  items-center rounded-xl w-[167px] h-11 absolute top-[80%]">
+                  <ul className="flex flex-col  gap-1 py-2 mr-14 ml-4">
+                    <li>Perfil</li>
+                    <li>Amigos</li>
+                    <li>Comunidade</li>
+                    <li>Configuração</li>
+                    <li className="flex items-center gap-1">
+                      Sair <LogOut className="size-4" />
+                    </li>
+                  </ul>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu.Root>
         </div>
       </header>
       <section className="bg-dark-50 w-full h-full">
