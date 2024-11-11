18f7d3a530e73a9fac9486c63637d2ec61779498
Jhuly Bastos <jhulytbds@gmail.com>
2024-11-11 13:23:15 -0300

    biografia

--- src/components/Biografia/index.tsx
+++ src/components/Biografia/index.tsx
@@ -1,5 +1,18 @@
 export function CardBiografia() {
   return (
-    <div className=" bg-dark-40 w-full h-screen rounded-2xl"> biografia</div>
+    <div className=" bg-dark-40 w-full h-screen rounded-2xl">
+      {" "}
+      biografia
+      <div>
+        <p>Nome:</p>
+        <p>Sobre você:</p>
+        <p>Relacionamento:</p>
+        <p>Aniversário:</p>
+        <p>Idade:</p>
+        <p>Profissão:</p>
+        <p>Estado:</p>
+        <p>Cidade:</p>
+      </div>
+    </div>
   );
 }
--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/navigation";
 import { CardBiografia } from "../Biografia";
 import CardCommunity from "../Community";
